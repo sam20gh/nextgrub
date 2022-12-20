b2f8bfb31e79b31b9f49139e6cd4c09da0696f4a
Sam Ghoreishi <info@grub24.co.uk>
2022-12-20 23:37:17 +0000

    footer changed

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,9 @@
 function Footer() {
     return (
         <footer className="layout-footer">
-            <p>Footer</p>
+            <p>My App 2021</p>
+            <li>   <a href="https://www.linkedin.com/in/andrew-lee-5b1b1b1b1/">LinkedIn</a></li>
+            <li>   <a href="https://github.com/andrewlee-1">GitHub</a></li>
         </footer>
     );
 }
