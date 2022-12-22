11ec7d32f5df42c958fa9962a9d8adb06a049eef
Sam Ghoreishi <info@grub24.co.uk>
2022-12-22 14:29:08 +0000

    home page added

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,24 @@ import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
+const banner ={
+    transform: 'translate3d(0, 570.413, 0)',
+    position: 'absolute',
+    height: 1194,
+    width: 1792,
+    maxWidth: 'none'
+  
+}
+const parallax = {
+  visibility: 'visible',
+  zIndex: -100,
+  position: 'fixed', 
+  top: 0,
+  left: 0, 
+  overflow: 'hidden',  
+  height: 814, 
+  width: 1792
+}
 
 export default function Home() {
   return (
@@ -15,107 +33,24 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
+      <div className="sectino1 parallax-container parallax-home parallax_gen" id="Section-1" data-natural-width="1000" data-parallax="scroll" data-position="top right" data-bleed="1" data-image-src="https://d3o47ov0yc40fm.cloudfront.net/upload/homebgntiny.webp" data-mirror-container="#Section-1">
+        <div className="parallax-mirror" style={parallax}><img className="parallax-slider" src="https://d3o47ov0yc40fm.cloudfront.net/upload/homebgntiny.webp" style={banner} /></div>
+        <div className="container s2">
+          <div className="Form-Section-1">
+
+            <h2 className="rotatetxting fs1h3">A better way to order Pizza
+            </h2>
+            <p>Delivery Address</p>
+            <button className="btn-search-sec1">Search</button>
+            <p>
+              <span>
+                <a className="cp btnfilt" data-target="#loginfrmmaster" data-toggle="modal">Login</a>
+              </span>
+              for your recent addresses.
+            </p>
           </div>
+          <a className="sphome cp" href="/browse">view popular restaurants and stores </a>
         </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
         </div>
       </main>
     </>
